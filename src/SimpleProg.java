99ae206e733a7c179f27685dbf8a25cbfa942ec2
mohamedhassan218 <m.hassan.def@gmail.com>
2021-10-23 11:44:02 +0200

    Projects haven't been ended yet

---
+++ src/SimpleProg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SimpleProg {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Please enter 2 characters !");
+		Scanner input = new Scanner(System.in);
+		String CHAR1 = input.nextLine() ;
+		String CHAR2 = input.nextLine() ;
+		
+	   
+
+	}
+
+}
