21ae0d88e94c99666522ca27642fb6723ee5b489
mohamedhassan218 <m.hassan.def@gmail.com>
2021-10-26 19:17:44 +0200

    Answers Part 1

---
+++ src/Sheet1_G1.java
@@ -0,0 +1,70 @@
+import java.util.Scanner ;
+public class Sheet1_G1 {
+
+	public static void main(String[] args) {
+		
+		
+
+			
+				// Odd or Even
+				/*int Number = 0 ;
+		        Scanner input = new Scanner(System.in);
+		        System.out.println("Please enter an integer!");
+		        Number = input.nextInt() ;
+		        if (Number <= 0 )
+		        {
+		        	System.out.println("NOT INTEGER");
+		        }
+		        else
+		        {
+		        	if (Number % 2 == 0)
+		        	{
+		        		System.out.println("Even!");
+		        	}
+		        	else
+		        	{
+		        		System.out.println("Odd!");
+		        	}
+		        }*/
+		//*****************************************************************************
+		        
+				// (Multiples)
+				/*int num1 = 0;
+				int num2 = 0;
+				Scanner input = new Scanner(System.in);
+				System.out.println("Please enter two integers!");
+				num1 = input.nextInt();
+				num2 = input.nextInt();
+		        if (num2 % num1 == 0)
+		        {
+		        	System.out.println("The first integer is a multiple of the seconed integer!");
+		        }
+		        else 
+		        {
+		        	System.out.println("The first integer is not a multiple of the seconed integer!");
+		        }*/
+		        
+		//*******************************************************************************
+		        
+		        // ((Checkerboard Pattern of Asterisks)
+				/*for (int i =0; i<9;i++)
+				{
+					System.out.println("* * * * * * * ");
+					System.out.println(" * * * * * * * ");
+				}*/
+				
+		//*******************************************************************************
+				
+				
+				
+				
+				
+				
+				
+				
+		        
+			}
+
+		}
+
+
---
+++ src/Sheet1_G2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner ;
+public class Sheet1_G2 {
+
+	public static void main(String[] args) {
+		
+		// 11111111
+		/*String Name ;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter your name!");
+		Name = input.nextLine() ;
+		System.out.println("Hello " + Name ); */
+//****************************************************************
+		
+		//2222222
+		/*int xCor ;
+		int yCor ;
+		System.out.println("Please enter the coordinates : ");
+		Scanner input = new Scanner(System.in);
+		xCor = input.nextInt();
+		yCor = input.nextInt();
+		if (xCor >= 2 && xCor <=45)
+		{
+			if (yCor >= 25 && yCor <= 50)
+			{
+				System.out.println("The ball is above the line !");
+			}
+			else 
+			{
+				System.out.println("IS NOT ABOVE THE LINE @@@");
+			}
+		}
+		else 
+		{
+			System.out.println("IS NOT ABOVE THE LINE @@@");
+		}*/
+//*******************************************************************
+		
+		
+		
+	}
+
+}
