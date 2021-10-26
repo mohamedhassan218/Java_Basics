c1d98a47c4c5c75eb84f66ec3692fc802820d49f
mohamedhassan218 <m.hassan.def@gmail.com>
2021-10-26 21:35:31 +0200

    Final answers

--- src/Sheet1_G1.java
+++ src/Sheet1_G1.java
@@ -3,8 +3,6 @@ public class Sheet1_G1 {
 
 	public static void main(String[] args) {
 		
-		
-
 			
 				// Odd or Even
 				/*int Number = 0 ;
@@ -54,12 +52,130 @@ public class Sheet1_G1 {
 				}*/
 				
 		//*******************************************************************************
+				// (Separating the digits in an integer)
+		
+		// create Scanner to obtain input from command window
+				/*Scanner input = new Scanner(System.in);
 				
+				// initialize variables
+				int num;	// user input
 				
+				System.out.print("Enter a number with five digits: ");	// prompt
+				num = input.nextInt();	// read five digits input
 				
+				// ensure input number with five digits
+				if ( (num >= 10000) && (num <= 99999) )
+				{
+				System.out.printf("%d   ", (num / 10000));
+				System.out.printf("%d   ", (num / 1000) % 10);
+				System.out.printf("%d   ", (num / 100) % 10);
+				System.out.printf("%d   ", (num % 100) / 10);
+				System.out.printf("%d   ", (num % 10));
+				}
 				
+				// if entered number more than five digits
+				if (num > 99999)
+					System.out.println("You had entered a number more than five digits.");
 				
+				// if entered number less than five digits
+				if (num <= 9999)
+					System.out.println("You had entered a number less than five digits.");*/
+
+//**********************************************************************************************				
+ 
+				// (Tables of squares and cubes)
 				
+				// initialize variables
+				/*int num;	// number for square and cube calculation
+				
+				System.out.println("number"+"\t"+"square"+"\t"+"cube");
+				
+				num = 0;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 1;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 2;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 3;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 4;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 5;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 6;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 7;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 8;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 9;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));
+				
+				num = 10;
+				System.out.printf("%d \t %d \t %d\n", (num), (num * num), (num * num * num));	*/			
+				
+//*************************************************************************************************
+		
+		// (Largest and smallest integers)
+		/*Scanner input = new Scanner(System.in);
+        int number1, 
+            number2, 
+            number3, 
+            number4, 
+            number5,
+            largest,
+            smallest;
+
+        System.out.print("Enter first integer: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        number2 = input.nextInt();
+
+        System.out.print("Enter third integer: ");
+        number3 = input.nextInt();
+
+        System.out.print("Enter fourth integer: ");
+        number4 = input.nextInt();
+
+        System.out.print("Enter fifth integer: ");
+        number5 = input.nextInt();
+
+        largest = number1;
+        smallest = number1;
+
+        if (number2 > largest)
+            largest = number2;
+        if (number3 > largest)
+            largest = number3;
+        if (number4 > largest)
+            largest = number4;
+        if (number5 > largest)
+            largest = number5;
+
+        if (number2 < smallest)
+            largest = number2;
+        if (number3 < smallest)
+            largest = number3;
+        if (number4 < smallest)
+            largest = number4;
+        if (number5 < smallest)
+            largest = number5;
+
+        System.out.printf("Largest of five integers is %d, and smallest is %d%n", largest, smallest);*/
+				
+//*******************************************************************************************************
+		
+		
 				
 				
 		        
--- src/Sheet1_G2.java
+++ src/Sheet1_G2.java
@@ -1,9 +1,10 @@
 import java.util.Scanner ;
+import java.util.Random; 
 public class Sheet1_G2 {
 
 	public static void main(String[] args) {
 		
-		// 11111111
+		// 1
 		/*String Name ;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter your name!");
@@ -11,7 +12,7 @@ public class Sheet1_G2 {
 		System.out.println("Hello " + Name ); */
 //****************************************************************
 		
-		//2222222
+		//22
 		/*int xCor ;
 		int yCor ;
 		System.out.println("Please enter the coordinates : ");
@@ -35,8 +36,133 @@ public class Sheet1_G2 {
 		}*/
 //*******************************************************************
 		
+		//333
+		Scanner input = new Scanner(System.in);
+		double number1 ;
+		double number2 ;
+		System.out.println("Hi, What is the sum of this two integers ?!");
+		Random rand = new Random();
+		number1 = rand.nextDouble()*10;
+		number2 = rand.nextDouble()*10;
+		number1 = (int)number1;
+		number2 = (int)number2;
+		System.out.println(number1 + "  \t " + number2 );
+		double sumOfTheKid = input.nextInt();
+		double realSum = number1 + number2 ;
+		if (realSum == sumOfTheKid)
+		{
+			System.out.println("Correct!");
+		}
+		else
+		{
+			System.out.println("Not correct!");
+		}
 		
+		
+		
+//*********************************************************************
+		
+		//4444
+		/*boolean ch1 ;
+		boolean ch2 ;
+		System.out.println("Please enter A then B to show you the truth table of them!");
+		Scanner input = new Scanner(System.in);
+		ch1 = input.nextBoolean();
+		ch2 = input.nextBoolean();
+		
+		System.out.println("A and B is: " + (ch1 & ch2));
+		System.out.println("A or B is: " + (ch1 | ch2));
+		System.out.println("A xor B is: " + (ch1 ^ ch2));*/
+		
+//*********************************************************************
+		
+		//55555
+		
+		/*char C ='A';
+		int ASCII =  C;
+		for (int i =1 ; i <9 ;i++)
+		{
+			
+	        System.out.println(ASCII);            //Automatic casting
+		    ASCII ++ ;
+		}*/
+		
+		/*char C = 'A' ;
+		int ASCII = (int) C ;
+		for (int i =1 ; i <9 ;i++)
+		{	
+	        System.out.println(ASCII);           //Manual casting 
+		    ASCII ++ ;
+		}*/
+		
+//**********************************************************************
+		
+		//666666
+		/*Scanner input = new Scanner(System.in);
+		System.out.println("Hi, please enter three integers !");
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		a = input.nextInt();
+		b = input.nextInt();
+		c = input.nextInt();
+		int biggest =0;
+		int bigger =0;
+		int small =0;
+		if (a > b)
+		{
+			if (a > c)
+			{
+				biggest =a;
+				if (b > c)
+				{
+					bigger = b;
+					small = c;
+				}
+				else
+				{
+					bigger = c;
+					small = b;
+				}
+			}
+			else
+			{
+				biggest = c;
+				bigger = a;
+				small = b;
+			}
+		}
+		else
+		{
+			if (b > c)
+			{
+				biggest = b;
+				if (c > a)
+				{
+					bigger = c;
+					small =a;
+				}
+				else
+				{
+					bigger = a;
+					small =c;
+				}
+			}
+			else
+			{
+				biggest = c;
+				bigger = b;
+				small = a;
+			}
+		}
+		
+		System.out.println(small);
+		System.out.println(bigger);
+		System.out.println(biggest);*/
+
+//*******************************************************************
 		
 	}
+
 
 }
