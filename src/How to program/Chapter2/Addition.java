32ef9b5994448eebe4fe2bd15f45e544e8d12e0a
mohamedhassan218 <m.hassan.def@gmail.com>
2021-11-12 14:42:42 +0200

    partion the repo

--- src/How to program/Chapter2/Addition.java
+++ src/How to program/Chapter2/Addition.java
@@ -6,9 +6,7 @@ public class Addition {
         int number1; // first number to add
         int number2; // second number to add
         int sum; // sum of number1 and number2
-        System.out.print("Enter first integer: "); // prompt
         number1 = input.nextInt(); // read first number from user
-        System.out.print("Enter seconed integer: "); // prompt
         number2 = input.nextInt(); // read second number from user
         sum = number1 + number2; // add numbers, then store total in sum
         System.out.printf("Sum is %d%n", sum); // display sum
