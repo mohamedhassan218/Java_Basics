d750a2cb1c43d0ae920d39ddcd14a2adb55ecc6a
mohamedhassan218 <m.hassan.def@gmail.com>
2021-11-13 14:12:29 +0200

    Most Ch3

--- src/How to program/Chapter3/Account.java
+++ src/How to program/Chapter3/Account.java
@@ -1,16 +1,40 @@
 public class Account {
-    private String name;// instance variable
+    // instance variable
+    private String name;
+    private double balance;
 
-    
+    //constructor initializes name
+    public Account(String name , double balance) //Constructor's name is the class name
+    {
+        this.name = name;
+
+        if (balance > 0.0)
+        {
+            this.balance = balance;
+        }
+    }
+
     public void  setName(String name)
     {
-        this.name = name;                //method to set the name
+        this.name = name;        //method to set the name
     }
     
     
     public String getName()
     {
-        return name;                     //method to get the name
+        return name;            //method to get the name
+    }
+
+    public void deposite(double depositeAmount)
+    {
+        if (depositeAmount > 0.0)
+        {
+            balance += depositeAmount;
+        }
+    }
+    public double getBalance()
+    {
+        return balance;
     }
 
 }//end of class
--- src/How to program/Chapter3/AccountTest.java
+++ src/How to program/Chapter3/AccountTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class AccountTest {
     public static void main(String[] args) {
         
-        Scanner input = new Scanner (System.in);
+        /*Scanner input = new Scanner (System.in);
 
         Account myAccount = new Account();
 
@@ -11,9 +11,48 @@ public class AccountTest {
         String theName = input.nextLine();
         myAccount.setName(theName);
         System.out.println();
-        System.out.printf("Name in myAccount is: %s%n", myAccount.getName());
+        System.out.printf("Name in myAccount is: %s%n", myAccount.getName());*/
+
         /*While using printf, if we put %S (upper case ) the string will be in the upper case
         if we put %s (lower case ) the string will be in the lower case*/
+
+        //create two account object
+        Account account1 = new Account("Mohamed Hassan" , 50.00);
+        Account account2 = new Account("Mohamed Salah" , -7.53);
+        
+        //Display initial value for name and balance in each object
+        
+        System.out.printf("account1 balance is: %s%n" , account1.getBalance());
+        System.out.printf("account2 balance is: %s%n" , account2.getBalance());
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter deposite amount for account1: ");
+        double depositeAmount = input.nextDouble();
+        account1.deposite(depositeAmount);
+
+        System.out.print("Enter deposite amount for account2: ");
+        depositeAmount = input.nextDouble();
+        account2.deposite(depositeAmount);
+
+        //Final display
+        System.out.printf("account1:-  the name: %s , the final balance: %.2f . %n" , account1.getName() ,account1.getBalance());
+        System.out.printf("account2:-  the name: %s , the final balance: %.2f . %n" , account2.getName() ,account2.getBalance());
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
 
     }
     
