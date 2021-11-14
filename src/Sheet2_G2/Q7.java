e2e90e27790cc250922856c466968946203f5f5f
mohamedhassan218 <m.hassan.def@gmail.com>
2021-11-14 17:04:16 +0200

    Solution for Q7

--- src/Sheet2_G2/Q7.java
+++ src/Sheet2_G2/Q7.java
@@ -3,7 +3,7 @@ public class Q7
 {
     public static void main (String[] args)
     {
-		String number ;
+		/*String number ;
 		int sum =0, product =1;
 		boolean swi = true;
 		Scanner input = new Scanner(System.in);
@@ -28,8 +28,28 @@ public class Q7
 		}
 		
 		System.out.println("The sum is " + sum);
-		System.out.println("The product is " + product);
+		System.out.println("The product is " + product);*/
 
+
+		String choice = "";
+        int sum = 0;
+        int product = 1;
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+
+        while(!choice.equals("q")){
+            System.out.println("Enter a number or q to quit");
+            choice = input.next();
+
+            if(!choice.equals("q")){
+            int number = Integer.parseInt(choice);
+            sum = sum+number;
+            product = product*number;
+            count++;
+            }
+        } 
+		System.out.println("The sum is " + sum);
+		System.out.println("The product is " + product);
 
     }
     
