f4c55edb670f75fe0eaf6121e7c5e226a9dc1277
mohamedhassan218 <m.hassan.def@gmail.com>
2021-12-27 17:17:10 +0200

    Practice test answers but Q2 not finished

---
+++ src/How to program/Abstract/src/PracticeTest.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class PracticeTest 
+{
+	public static void main(String[] args) 
+    {
+		
+		//Problem for group 2: 
+		/*
+		 * int x; int k; double total = 0;
+		 * 
+		 * Scanner input = new Scanner(System.in);
+		 * 
+		 * 
+		 * System.out.print("Enter X: "); x = input.nextInt(); System.out.println();
+		 * 
+		 * System.out.print("Enter K: "); k = input.nextInt(); System.out.println();
+		 * 
+		 * for(int i = 1; i <= k; i++) { if (i%2 == 0) { total -= (Math.pow(x, i) / i);
+		 * } else if (i%2 == 1) { total += (Math.pow(x, i) / i); } }
+		 * 
+		 * total = Math.abs(total); System.out.println(total);
+		 */
+		
+		//******************************************************************************
+		//Problem for group 1:
+		int x ;
+	    int k;
+		double total = 0;
+	    
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter X: "); 
+	    x = input.nextInt(); 
+	    System.out.println();
+		
+	    System.out.print("Enter K: "); 
+	    k = input.nextInt(); 
+	    System.out.println();
+	    
+	    int denominator = 1;
+	    for(int i = 1; i <= k; i++)
+	    {	
+	    	total += (Math.pow(x, i) / (denominator * i));
+	        denominator *= i;
+	    }
+	    
+		System.out.println(total + 1 );
+    }
+
+}
